197c2873b36597a0e5f581b698bc1896601bc3af
DANRAH96 <216382567+DANRAH96@users.noreply.github.com>
2025-07-10 20:42:34 +0200

    Add function for round and game

--- script.js
+++ script.js
@@ -15,23 +15,84 @@ function getComputerChoice(){
     }else{
         computerChoice = "scissor";
     }
+    console.log(computerChoice);
 };
 
-getComputerChoice();
 
-console.log(computerChoice);
+
+
 
 //INPUT userChoice
+//TRANSFORM userChoice to lowercase
 let humanChoice;
 function getHumanChoice(){
     humanChoice = prompt("Enter your choice").toLowerCase();
     console.log(humanChoice);
 }
-getHumanChoice();
-//TRANSFORM userChoice to lowercase
-//COMPARE userChoice and computerChoice
-//IF userChoice beats computerChoice THEN userScore++
-//IF ELSE computerChoice beats userChoice computerScore++
-//ELSE draw, no score
+
+
+//INIT score variables
+let computerScore = 0;
+let userScore = 0;
+
+//FUNCTION to play a round
+function playRound(humanChoice, computerChoice){
+    
+ //COMPARE userChoice and computerChoice  
+  
+
+    switch(true){
+        case (humanChoice == computerChoice):
+            console.log("Draw");
+            break;
+        case(humanChoice == "rock" && computerChoice == "scissor"):
+            console.log("You win the round");
+            userScore++;
+            break;
+        case(humanChoice == "rock" && computerChoice =="paper"):
+            console.log("Computer win the round");
+            computerScore++;
+            break;
+        case(humanChoice == "paper" && computerChoice =="rock"):
+            console.log("You win the round");
+            userScore++;
+            break;
+        case(humanChoice == "paper" && computerChoice =="scissor"):
+            console.log("Computer win the round");
+            computerScore++;
+            break;
+        case(humanChoice == "scissor" && computerChoice =="paper"):
+            console.log("You win the round");
+            userScore++;
+            break;
+         case(humanChoice == "scissor" && computerChoice =="rock"):
+            console.log("Computer win the round");
+            computerScore++;
+            break;
+        
+    }
+}
+
+
+
+let numberOfRounds = parseInt(prompt("How many rounds do you want to play?"));
+function playGame(numberOfRounds){
+    for(let i=0; i<numberOfRounds; i++){
+        getComputerChoice();
+        getHumanChoice();
+
+        playRound(humanChoice, computerChoice);
+        console.log(userScore);
+        console.log(computerScore); 
+    }
+    if(userScore > computerScore){
+        console.log("User wins the game!");
+    }else if(computerScore > userScore){
+        console.log("Computer wins the game");
+    }else {
+        console.log("It's a draw!");
+    }
+}
+playGame(numberOfRounds);
 //ITERATE 5 times
 //OUTPUT winner
