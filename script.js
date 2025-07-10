ffc2d359264a018a349a86020b33ca0c7dd0ad02
DANRAH96 <216382567+DANRAH96@users.noreply.github.com>
2025-07-10 20:18:50 +0200

    Add function to input user choice

--- script.js
+++ script.js
@@ -4,26 +4,30 @@
 
 //INIT computerChoice
 let computerChoice;
+//ASSIGN random value to computerChoice
 function getComputerChoice(){
     let randomValue = Math.floor(Math.random()*3);
     
     if(randomValue === 0){
         computerChoice = "rock";
-        //return computerChoice;
     }else if(randomValue === 1){
         computerChoice ="paper";
-        //return computerChoice;
     }else{
         computerChoice = "scissor";
-        //return computerChoice;
     }
 };
 
 getComputerChoice();
 
 console.log(computerChoice);
-//ASSIGN random value to computerChoice
+
 //INPUT userChoice
+let humanChoice;
+function getHumanChoice(){
+    humanChoice = prompt("Enter your choice").toLowerCase();
+    console.log(humanChoice);
+}
+getHumanChoice();
 //TRANSFORM userChoice to lowercase
 //COMPARE userChoice and computerChoice
 //IF userChoice beats computerChoice THEN userScore++
